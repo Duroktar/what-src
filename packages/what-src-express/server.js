43b9716df997fb7485ed524b825ea97807fff24f
traBpUkciP <scott.doucet@hotmail.com>
2019-09-21 20:18:02 -0300

    add fallback for when parsing env values

--- packages/what-src-express/server.js
+++ packages/what-src-express/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const host = process.env.WHAT_SRC_DAEMON_HOST || 'localhost'
 const port = process.env.WHAT_SRC_DAEMON_PORT || 8018
 const endpoint = process.env.WHAT_SRC_DAEMON_ENDPOINT || '__what_src'
-const shh = JSON.parse(process.env.WHAT_SRC_DAEMON_SHH) === true
+const shh = JSON.parse(process.env.WHAT_SRC_DAEMON_SHH || 'false') === true
 const editor = process.env.WHAT_SRC_DAEMON_EDITOR || 'vscode'
 
 if (!process.env.EDITOR) {
